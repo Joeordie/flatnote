1e3c97d6b69bb198eb149ecf79e4c21e5c81c4ae
joeordie <joeordie@HP17Antegros.joeordie.com>
2016-06-25 07:43:47 -0700

    initial

---
+++ src/flatnote/FlatNote.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flatnote;
+
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author joeordie
+ */
+public class FlatNote {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmMainWindow main = new FrmMainWindow();
+        
+      
+        
+    }
+    public static void NewNotebook(String varNotbookloc){
+            
+    }
+    public static void NewNote(String varNoteName){
+            
+    }
+    public static void SaveNote(Object NoteComp){
+            
+    }
+    public static void Search(){
+            
+        }
+    public static void Delete(String varNotbookloc){
+            
+        }
+    
+}
